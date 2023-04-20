ba3d465f614e817232539a0591fb05ad91ef68a1
Sirjoon Mandi <121184868+sirjonmandi@users.noreply.github.com>
2023-04-20 12:36:01 +0530

    file uploaded :)

---
+++ main.js
@@ -0,0 +1,35 @@
+alert("optimized for desktop only");
+
+var data = document.getElementById('data');
+var submit = document.getElementById('submit');
+var list = document.querySelector(".list-area");
+
+submit.addEventListener('click', function() {
+    if(data.value == "") {
+        console.log("no data invoked");
+    }else{
+        addList(data);
+        data.value = "";
+    }
+})
+
+var addList = (data) => {
+    var listItem = document.createElement("li");
+    listItem.innerHTML = `
+    ${data.value}
+    <i class="fas fa-times"></i>
+    `;
+    listItem.addEventListener("click",
+     function () {
+        this.classList.toggle("done");
+     }
+    )
+
+    listItem.querySelector("i").addEventListener(
+        "click",
+        function(){
+            listItem.remove();
+        }
+    )
+    list.appendChild(listItem);
+}
